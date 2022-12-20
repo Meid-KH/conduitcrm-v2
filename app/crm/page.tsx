21864b443d3bbec09d44817aebf372652518e2d3
Mohamed <maeid.kh@gmail.com>
2022-12-20 19:12:54 +0100

    - Reusable title component.

---
+++ app/crm/page.tsx
@@ -0,0 +1,14 @@
+import { Title } from "components/ui";
+import React from "react";
+
+const page = () => {
+	return (
+		<div className="container my-96">
+			<Title as="h2" size="7xl" color="whitePurple" dir="center">
+				Feature - CRM Lorem ipsum dolor sit amet consectetur.
+			</Title>
+		</div>
+	);
+};
+
+export default page;
---
+++ components/ui/Title.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { classNames } from "utils/classNames";
+
+const Title: React.FC<{
+	size?:
+		| "xs"
+		| "sm"
+		| "base"
+		| "md"
+		| "lg"
+		| "2xl"
+		| "3xl"
+		| "4xl"
+		| "5xl"
+		| "7xl"
+		| "8xl"
+		| "9xl";
+	color: string;
+	dir?: string;
+	as?: React.ElementType;
+	children: React.ReactNode | React.ReactNode[];
+	className?: string;
+}> = ({
+	size = "7xl",
+	color = "red",
+	dir,
+	as: Tag = "h1",
+	children,
+	className,
+}) => {
+	const colors: { [key: string]: string } = {
+		red: "text-transparent bg-clip-text bg-gradient-to-r from-danger-300 to-danger-200",
+		blue: "font-medium__ text-transparent bg-clip-text bg-gradient-to-r from-teal via-info to-primary",
+		whitePurple:
+			"text-transparent bg-clip-text bg-gradient-to-r from-light via-purple to-danger-200",
+	};
+	return (
+		<Tag
+			className={classNames(
+				"leading-tight font-medium tracking-wide",
+				dir && `text-${dir}`,
+				`text-${size}`,
+				colors[color],
+				className
+			)}
+		>
+			{children}
+		</Tag>
+	);
+};
+
+export default Title;
